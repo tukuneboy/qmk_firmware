96625a3c6a432d7d41c1dfe00532df84e7b072db
Salicylic-acid3 <46864619+Salicylic-acid3@users.noreply.github.com>
2022-12-06 17:26:09 +0900

    リポジトリ再構築のため再アップロード

--- keyboards/salicylic_acid3/naked64/rev2/keymaps/led/config.h
+++ keyboards/salicylic_acid3/naked64/rev2/keymaps/led/config.h
@@ -18,14 +18,38 @@
 
 /* Select hand configuration */
 
-#define MASTER_LEFT
-// #define MASTER_RIGHT
-// #define EE_HANDS
-#define USE_SERIAL_PD2
-
 #define TAPPING_FORCE_HOLD
 #define TAPPING_TERM 180
+
+#define RGB_DI_PIN D0
 
 #define RGBLED_NUM 12    // Number of LEDs
 #define RGBLIGHT_LED_MAP { 0, 1, 3, 2, 2, 3, 2, 3, 4, 5, 6, 7 }
 
+#ifndef IOS_DEVICE_ENABLE
+  #define RGBLIGHT_LIMIT_VAL 180
+  #define RGBLIGHT_VAL_STEP 17
+#else
+  #define RGBLIGHT_LIMIT_VAL 50
+  #define RGBLIGHT_VAL_STEP 4
+#endif
+#define RGBLIGHT_HUE_STEP 10
+#define RGBLIGHT_SAT_STEP 17
+
+// Selection of RGBLIGHT MODE to use.
+#define RGBLIGHT_EFFECT_BREATHING
+#define RGBLIGHT_EFFECT_SNAKE
+
+#if defined(RGBLIGHT_ENABLE) && !defined(IOS_DEVICE_ENABLE)
+// USB_MAX_POWER_CONSUMPTION value for naked48 keyboard
+//  120  RGBoff, OLEDoff
+//  120  OLED
+//  330  RGB 6
+//  300  RGB 32
+//  310  OLED & RGB 32
+  #define USB_MAX_POWER_CONSUMPTION 400
+#else
+  // fix iPhone and iPad power adapter issue
+  // iOS device need lessthan 100
+  #define USB_MAX_POWER_CONSUMPTION 100
+#endif
--- keyboards/salicylic_acid3/naked64/rev2/keymaps/trackball/config.h
+++ keyboards/salicylic_acid3/naked64/rev2/keymaps/trackball/config.h
@@ -18,10 +18,5 @@
 
 /* Select hand configuration */
 
-#define MASTER_LEFT
-// #define MASTER_RIGHT
-// #define EE_HANDS
-#define USE_SERIAL_PD2
-
 #define TAPPING_FORCE_HOLD
 #define TAPPING_TERM 180
