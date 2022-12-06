a4f080168f6074af2b9154107486aac80b40a830
Salicylic-acid3 <46864619+Salicylic-acid3@users.noreply.github.com>
2022-12-06 21:10:08 +0900

    Update to the latest writing style

--- keyboards/salicylic_acid3/keyfuda/01_tenkey/keymaps/via/keymap.c
+++ keyboards/salicylic_acid3/keyfuda/01_tenkey/keymaps/via/keymap.c
@@ -20,7 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT(
       //|-----------------------------------|
-          KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS,
+           KC_NUM, KC_PSLS, KC_PAST, KC_PMNS,
       //|--------+--------+--------+--------|
             KC_P7,   KC_P8,   KC_P9, KC_PPLS,        
       //|--------+--------+--------+--------|
